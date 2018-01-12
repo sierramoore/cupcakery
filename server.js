ffc8fcb3e6d6bd74c381d53f7c0947a82c01df1f
sergio <scrummish@gmail.com>
2018-01-12 12:45:54 -0600

    created a form to create new cupcakes

--- server.js
+++ server.js
@@ -4,17 +4,35 @@ const mongoose = require('mongoose');
 const port = 3000;
 const bodyParser = require('body-parser');
 
+mongoose.connect('mongodb://localhost:27017/cupcakes',{useMongoClient: true});
+mongoose.connection.once('open', ()=>{
+	console.log('working mongo');
+})
+
 app.use(bodyParser.urlencoded({ extend: true}));
 
 const Cupcake = require('./models/cupcakes.js');
 
 app.get('/cupcakes', (req,res) =>{
-    console.log("works")
-    Cupcake.create(req.body,(err,createCupcake)=>{
+	res.render('index.ejs');
+    
+})
+app.post('/cupcakes', (req,res)=>{
+	if (req.body.delicious === 'on'){
+		req.body.delicious = true;
+	} else req.body.delicious = false;
 
-        console.log("works")
+	Cupcake.create(req.body,(err,createCupcake)=>{
+        console.log("created: " + createCupcake)
     })
-});
+})
+
+app.get('/cupcakes/new', (req,res) =>{
+	res.render('new.ejs');
+    // Cupcake.create(req.body,(err,createCupcake)=>{
+    //     console.log(req.body);
+    // })
+})
 
 app.listen(port, (req, res) =>{
     console.log("listening on ", port)
